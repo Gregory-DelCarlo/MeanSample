74a5ca7c7abb6ea8c87d7aa3ffb48c29e07ff61f
Gregory-DelCarlo <ethandelcarlo@gmail.com>
2022-09-04 16:34:10 -0700

    Setup employee actions

---
+++ client/src/app/employees/employee.ts
@@ -0,0 +1,7 @@
+// generated using ng g interface employee
+export interface Employee { //defines the properties of the employee object
+    name?: string;
+    position?: string;
+    level?: 'junior' | 'mid' | 'senior';
+    id?: string;
+}
