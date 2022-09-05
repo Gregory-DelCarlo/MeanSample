f91a9d9611cbc4cadd152e11a09ca33c106ab0c7
Gregory-DelCarlo <ethandelcarlo@gmail.com>
2022-09-04 18:39:38 -0700

    Added Routing

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AddEmployeeComponent } from './employees/components/add-employee.component';
+import { EditEmployeeComponent } from './employees/components/edit-employee.component';
 import { EmployeesListComponent } from './employees/components/employees-list.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'employees', pathMatch: 'full' },
   { path: 'employees', component: EmployeesListComponent },
+  { path: 'employees/new', component: AddEmployeeComponent },
+  { path: 'employees/edit/:id', component: EditEmployeeComponent }
 ];
 
 @NgModule({
